ce3d0b5397e73e21d46720db7192c7470f9949c0
gabrielgws.dev <gabrielgws.dev@gmail.com>
2024-04-15 13:01:45 -0300

    feat: finish app

--- src/components/Shared/Header.tsx
+++ src/components/Shared/Header.tsx
@@ -1,11 +1,14 @@
 import LogoA1 from '@/assets/logo-A1'
 import { ModeToggle } from './ModeToggle'
 import { MenuBar } from './MenuBar'
+import Link from 'next/link'
 
 export const Header = () => {
   return (
     <div className="fixed top-0 z-50 flex h-16 w-full items-center justify-between bg-primary px-5">
-      <LogoA1 className="h-[30px] w-[30px] fill-primary-foreground" />
+      <Link href="/">
+        <LogoA1 className="h-[30px] w-[30px] fill-primary-foreground" />
+      </Link>
 
       <div className="flex flex-row items-center gap-4">
         <ModeToggle />
